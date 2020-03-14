617fef28d5cd44c5ed9223eaeb4cfc39299852be
mozvid <wiidaryanto@gmail.com>
2020-03-14 20:07:08 +0700

    Update combine-aev6.js

--- combine-aev6.js
+++ combine-aev6.js
@@ -2,12 +2,13 @@ let domain	= window.location.hostname,
 		content = document.getElementById("desc"),
 		ua 		  = navigator.userAgent,
 		s 		  = document.createElement('script'),
-		regex 	  = '/google|bot|bing|yahoo|pinterest|yandex|facebook|webmaster|spider|crawlr/i',
-		productId = document.getElementById("productId").value;
-
+		regex 	= '/google|bot|bing|yahoo|pinterest|yandex|facebook|webmaster|spider|crawlr/i',
+		productId   = document.getElementById("productId").value;
+		affiliateId = document.getElementById("affiliateId").value;
+alert(affiliateId);
 if (!ua.match(regex)) {
-	var afflink = "https://s.click.aliexpress.com/deep_link.htm?dl_target_url=https%3A%2F%2Fwww.aliexpress.com%2Fitem%2F-%2F"+productId+".html&aff_short_key=bwU7vsvE&dp="+domain;
-	window.location.replace(afflink);
+//	var afflink = "https://s.click.aliexpress.com/deep_link.htm?dl_target_url=https%3A%2F%2Fwww.aliexpress.com%2Fitem%2F-%2F"+productId+".html&aff_short_key=bwU7vsvE&dp="+domain;
+//	window.location.replace(afflink);
 }
 
 s.type = 'text/javascript';
