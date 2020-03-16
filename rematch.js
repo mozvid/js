0274172e224364c9221c83c2e429935faa663eb5
mozvid <wiidaryanto@gmail.com>
2020-03-16 12:55:38 +0700

    Update rematch.js

--- rematch.js
+++ rematch.js
@@ -5,6 +5,7 @@ alert(url);
 if(!ua.match(regex)){    
   if(url.match('/?m=1')){
     var redirectUrl = url.replace('?m=1','?m=0');
-    window.location.replace(redirectUrl);
+      alert(redirectUrl);
+ //   window.location.replace(redirectUrl);
   }
 }
