be11b3cb0c7947cba6fb7a5c010eec3fb7c3ae67
mozvid <wiidaryanto@gmail.com>
2020-03-16 12:45:29 +0700

    Update rematch.js

--- rematch.js
+++ rematch.js
@@ -1,6 +1,7 @@
 let url = window.location.href,
     ua = navigator.userAgent,
     regex = '/google|bot|bing|yahoo|pinterest|yandex|facebook|webmaster|spider|crawlr/i';
+alert(url);
 if(!ua.match(regex)){    
   if(url.match('/?m=1')){
     var redirectUrl = url.replace('?m=1','?m=0');
