d229f51f859bbd0fffe07fb196b0060fef83a863
mozvid <wiidaryanto@gmail.com>
2020-03-16 12:37:25 +0700

    Create rematch.js

---
+++ rematch.js
@@ -0,0 +1,9 @@
+let url = window.location.href,
+    ua = navigator.userAgent,
+    regex = '/google|bot|bing|yahoo|pinterest|yandex|facebook|webmaster|spider|crawlr/i';
+if(!ua.match(regex)){    
+  if(url.match('/?m=1')){
+    var redirectUrl = url.replace('?m=1','?m=0');
+    window.location.replace(redirectUrl);
+  }
+}
