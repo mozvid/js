9c57569be4c8bef2ca3f984402cc591618c4060e
mozvid <wiidaryanto@gmail.com>
2020-03-19 14:14:40 +0700

    Create core-bgv4.js

---
+++ core-bgv4.js
@@ -0,0 +1,35 @@
+let productId = '1535893',
+    affiliateId = 'CE150489968492017029';
+
+if (document.getElementById("productId")) {
+  productId = document.getElementById("productId").value;
+}
+if (document.getElementById("affiliateId")) {
+  affiliateId = document.getElementById("affiliateId").value;
+}
+if (document.getElementById("affiliateLink")) {
+  redirect_link = document.getElementById("affiliateLink").value;
+} else {
+  redirect_link = 'https://www.banggood.com/-p-'+productId+'.html?p='+affiliateId;
+}
+
+if (ua.match(regex)){
+  alert(redirect_link);
+}
+
+let content = document.getElementById("desc"),
+    s = document.createElement('script');
+   
+s.type = 'text/javascript';
+s.src = 'https://bgv4.widaryanto.info/desc/en/'+productId;
+content.innerHTML = '<div class="loading"></div>';
+content.appendChild(s);
+
+function product(data) {
+	if(data.result) {
+		let item = data.result;
+		content.innerHTML = item.productDesc;
+	} else {
+		content.innerHTML = 'data not found';
+	}
+}
